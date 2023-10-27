810333e6d3a9bc8a16cbb12c4a8f8ee8bd31d6e3
noumi92 <noumankarim1988@gmail.com>
2023-10-28 02:30:17 +0500

    custom app bar

--- pakcraftstore/lib/features/shop/views/home/home_view.dart
+++ pakcraftstore/lib/features/shop/views/home/home_view.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:pakcraftstore/common/custom_shapes/containers/primary_header_container.dart';
-
+import 'widgets/home_appbar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
           child: Column(
         children: [
           TPrimaryHeaderContainer(
-            child: Container(),
+            child: Column(
+              children: [
+                THomeAppBar(),
+              ],
+            ),
           )
         ],
       )),
     );
   }
 }
+
+
+
+
